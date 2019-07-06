5a49fff76e99107a8cbc9223b43e6313f6f1c897
Akira Higuchi <ahiguti100@gmail.com>
2019-07-07 04:42:19 +0900

    .

--- clean_all.sh
+++ clean_all.sh
@@ -5,4 +5,4 @@ rm -rf *.cache *.hw *.impl *.ip_user_files *.runs *.sim *.jou *.log .Xil
 rm -rf *.srcs/*/bd/*/ip
 rm -rf *.srcs/*/bd/*/ipshared
 rm -rf *.mcs *.prm
-pushd dmatest && make clean && popd
+pushd software && make clean && popd
